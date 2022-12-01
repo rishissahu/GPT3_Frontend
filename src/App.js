13c585314fd0eafe2000c5268964e9ac69467255
Rishi Sahu <l.rishissahu@gmail.com>
2022-12-01 14:04:35 +0530

    Header created

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ const App = () => {
     <div className="App">
       <div className="gradient__bg">
         <Navbar/>
-        {/* <Header/> */}
+        <Header/>
       </div>
       <Brand/>
       <WhatGPT3/>
